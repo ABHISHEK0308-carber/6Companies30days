76a38e30b862679d5066d7277f1217b9739e001e
Abhishek goyal <96344136+ABHISHEK0308-carber@users.noreply.github.com>
2023-01-07 15:42:16 +0530

    Create Factorial_Trailing_Zeroes.cpp

---
+++ Goldman_Sachs/Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count =0;
+        for(int i=5; i<=n; i=i*5)
+        {
+            count= count+ n/i;
+        }
+        return count;
+    }
+};
