f6c4c1709144670819f7ce60145dc6fd6722da84
Abhishek goyal <96344136+ABHISHEK0308-carber@users.noreply.github.com>
2023-01-12 17:58:22 +0530

    Add files via upload

---
+++ Adobe/x.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution 
+{
+public:
+    int minMutation(string start, string end, vector<string>& bank) 
+    {
+        deque<pair<string,int>> q({{start,0}});
+        set<string> b(bank.begin(), bank.end());
+        vector<char> nuc = {'A', 'T', 'G', 'C'};  
+        while (!q.empty())
+        {
+            auto [g, m] = q.front(); q.pop_front();
+            if (g == end) return m;
+            
+            for (int i = 0; i < g.size(); ++i)
+                for (char n : nuc)
+                {
+                    string gm = g; gm[i] = n;
+                    if (b.count(gm))
+                    {
+                        b.erase(gm);
+                        q.emplace_back(gm, m+1);
+                    }
+                }
+        } 
+        return -1;
+    }
+};
